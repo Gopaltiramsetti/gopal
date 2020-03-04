d636c5f031be87a36d19271e1d00e82b0af46726
admin <admin@1A-1>
2020-03-04 06:54:04 -0500

    first commit

---
+++ usecase1/src/main/java/com/example/demo/dao/Employee.java
@@ -0,0 +1,49 @@
+package com.example.demo.dao;
+
+public class Employee {
+	private int empId;
+	private String empName;                                                                                                                                                                                                                                                                                                                                                                 
+	private String inTime;
+	private String outTime;
+	private String date;
+	private String locationName;
+	
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getInTime() {
+		return inTime;
+	}
+	public void setInTime(String inTime) {
+		this.inTime = inTime;
+	}
+	public String getOutTime() {
+		return outTime;
+	}
+	public void setOutTime(String outTime) {
+		this.outTime = outTime;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getLocationName() {
+		return locationName;
+	}
+	public void setLocationName(String locationName) {
+		this.locationName = locationName;
+	}
+	
+
+}
