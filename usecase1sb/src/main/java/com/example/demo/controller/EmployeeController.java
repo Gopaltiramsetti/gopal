e8d61a8968ddd1f57208c84eff51bbe04943e7c7
admin <admin@1A-1>
2020-03-11 18:22:52 +0530

    swagger

--- usecase1sb/src/main/java/com/example/demo/controller/EmployeeController.java
+++ usecase1sb/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.EmpDao;
 import com.example.demo.service.EmployeeService;
 
 @RestController
+
+@RequestMapping("/emp")
+
 public class EmployeeController {
 
 	@Autowired
